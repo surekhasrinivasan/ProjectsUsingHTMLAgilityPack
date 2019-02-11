ca2fe5545876214201a95fe37904ed0ec5dbb32a
Surekhasrinivasan <surekhasrao@gmail.com>
2019-02-11 12:13:03 -0500

    program prints the body of the html

--- LoadHTMLFromString/LoadHTMLFromString/Program.cs
+++ LoadHTMLFromString/LoadHTMLFromString/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,24 @@ namespace LoadHTMLFromString
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var html =
+                @"<!DOCTYPE html>
+                <html>
+                <body>
+	                <h1>This is <b>bold</b> heading</h1>
+	                <p>This is <u>underlined</u> paragraph</p>
+	                <h2>This is <i>italic</i> heading</h2>
+                </body>
+                </html> ";
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var htmlBody = htmlDoc.DocumentNode.SelectSingleNode("//body");
+
+            Console.WriteLine(htmlBody.OuterHtml);
         }
     }
 }
