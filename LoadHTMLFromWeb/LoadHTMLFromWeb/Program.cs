21190466ff3bdfcb3de501d5aa51f484a70e3f6c
Surekhasrinivasan <surekhasrao@gmail.com>
2019-02-12 18:03:23 -0500

    HTMLWeb.Load method gets an HTML document from Web

--- LoadHTMLFromWeb/LoadHTMLFromWeb/Program.cs
+++ LoadHTMLFromWeb/LoadHTMLFromWeb/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,17 @@ namespace LoadHTMLFromWeb
     // Program to show HTMLWeb.Load method gets an HTML document from Web 
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var html = @"http://html-agility-pack.net/";
+
+            HtmlWeb web = new HtmlWeb();
+
+            var htmlDoc = web.Load(html);
+
+            var node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
+
+            Console.WriteLine("Node name: " + node.Name + "\n" + node.OuterHtml);            
         }
     }
 }
