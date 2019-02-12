c7d99bd19b48fe3fa8aadd0db2d9ebfc5c9e971f
Surekhasrinivasan <surekhasrao@gmail.com>
2019-02-12 17:34:18 -0500

    Load HTML from Web using HTML Agility Pack - initial commit

---
+++ LoadHTMLFromWeb/LoadHTMLFromWeb/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadHTMLFromWeb
+{
+    // Program to show HTMLWeb.Load method gets an HTML document from Web 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
