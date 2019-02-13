6f98add1882fc0a3cc1734190c666b0baad4feaf
Surekhasrinivasan <surekhasrao@gmail.com>
2019-02-12 21:15:52 -0500

    Load HTML from browser using HTML Agility pack - initial commit

---
+++ LoadHTMLFromBrowser/LoadHTMLFromBrowser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadHTMLFromBrowser
+{
+    // Program to Load Html From Browser
+    // HtmlWeb.Load method gets an HTML document from a web browser.
+    // It makes it possible to wait for JavaScript to be run by customizing 
+    // the isBrowserScriptCompleted parameter.
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
