cf84dcc88209fd970a58946cfc93318b90a8f712
Surekhasrinivasan <surekhasrao@gmail.com>
2019-02-15 23:18:05 -0500

    Example program to load html from web using HtmlAgilityPack

---
+++ ProgramUsingHTMLAgilityPack/ProgramUsingHTMLAgilityPack/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramUsingHTMLAgilityPack
+{
+    // Program to load html page from web using HTMLAgilityPack
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
