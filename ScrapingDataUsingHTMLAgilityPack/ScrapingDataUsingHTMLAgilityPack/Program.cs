7d326558414eaeb591da9f6c2239d7683b10d3b9
Surekhasrinivasan <surekhasrao@gmail.com>
2019-02-15 19:23:59 -0500

    Example for Scraping data using HTML Agility pack

--- ScrapingDataUsingHTMLAgilityPack/ScrapingDataUsingHTMLAgilityPack/Program.cs
+++ ScrapingDataUsingHTMLAgilityPack/ScrapingDataUsingHTMLAgilityPack/Program.cs
@@ -12,9 +12,7 @@ namespace ScrapingDataUsingHTMLAgilityPack
         static void Main()
         {
             HtmlWeb web = new HtmlWeb();
-
-            //var html = @"https://www.google.com/search?q=book&source=lnms&tbm=bks&sa=X&ved=0ahUKEwjc_dHmyrngAhUHPN8KHYRaDPAQ_AUIDygC&biw=1366&bih=657";
-
+                        
             var htmlDoc = web.Load("http://www.yellowpages.com/search?search_terms=Software&geo_location_terms=Sydney2C+ND");
 
             var Titles = htmlDoc.DocumentNode.SelectNodes("//a[@class='business-name']").ToList();
