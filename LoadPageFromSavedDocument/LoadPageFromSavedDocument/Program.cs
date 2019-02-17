6ecf7241d2714f8f4987f382ba387bef1dbbd992
Surekhasrinivasan <surekhasrao@gmail.com>
2019-02-17 18:07:34 -0500

    Program get all the hyperlinks from the page

--- LoadPageFromSavedDocument/LoadPageFromSavedDocument/Program.cs
+++ LoadPageFromSavedDocument/LoadPageFromSavedDocument/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,16 @@ namespace LoadPageFromSavedDocument
     // Program to load page from saved document 
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            HtmlDocument document = new HtmlDocument();
+            document.Load(@"C:\Users\sreen\source\repos\HTMLAgilityPackProjects\LoadPageFromSavedDocument\Sample.txt");
+
+            var nodes = document.DocumentNode.SelectNodes("//a").ToArray();
+            foreach(var node in nodes)
+            {
+                Console.WriteLine(node.InnerHtml);
+            }
         }
     }
 }
