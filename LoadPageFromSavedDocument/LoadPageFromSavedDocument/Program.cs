eef237dc746699d681b441b3a1b098351b25a25f
Surekhasrinivasan <surekhasrao@gmail.com>
2019-02-17 17:14:23 -0500

    Load page from a saved document using html agility pack - initial commit

---
+++ LoadPageFromSavedDocument/LoadPageFromSavedDocument/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadPageFromSavedDocument
+{
+    // Program to load page from saved document 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
