b2b47cb0add81637e11b080b8e1778ebd92ad798
Surekhasrinivasan <surekhasrao@gmail.com>
2019-02-16 19:20:31 -0500

    prints all the author names who created the Recent Posts

--- ProgramUsingHTMLAgilityPack/ProgramUsingHTMLAgilityPack/Program.cs
+++ ProgramUsingHTMLAgilityPack/ProgramUsingHTMLAgilityPack/Program.cs
@@ -17,16 +17,24 @@ namespace ProgramUsingHTMLAgilityPack
 
             //Console.WriteLine(document.DocumentNode.OuterHtml);
 
-            var node = document.DocumentNode.SelectSingleNode("//head/title");
+            //var node = document.DocumentNode.SelectSingleNode("//head/title");
 
-            Console.WriteLine("Node Name: " + node.Name + "\n" + node.OuterHtml);
+            //Console.WriteLine("Node Name: " + node.Name + "\n" + node.OuterHtml);
 
-            var titles = document.DocumentNode.SelectNodes("//a[@class='title']").ToList();
+            //var titles = document.DocumentNode.SelectNodes("//a[@class='title']").ToList();
 
-            foreach(var title in titles)
+            //foreach(var title in titles)
+            //{
+            //    Console.WriteLine(title.InnerText);
+            //}
+
+            // Prints all the author names of the Recent Posts
+            var htmlNodes = document.DocumentNode.SelectNodes("//a[@class='author']").ToArray();
+
+            foreach(var node in htmlNodes)
             {
-                Console.WriteLine(title.InnerText);
-            }                
+                Console.WriteLine(node.InnerHtml);
+            }
         }
     }
 }
